781f3cd951b8947ba1f1179cce631393e57de383
dutchmartin <martijn.groeneveldt@gmail.com>
2019-07-11 23:39:54 +0200

    save progress

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 mod controllers;
 mod view_models;
+mod services;
 
 use actix_files as fs;
 use actix_web::*;
+use std::thread;
+use std::time::Duration;
 
 use crate::controllers::pages_controller::*;
+use crate::services::twitch_service;
 
 fn main() -> std::io::Result<()> {
+    thread::spawn(|| {
+        loop {
+            println!("Fetching info");
+            twitch_service::is_channel_online("nietrickbroers");
+            thread::sleep(Duration::from_millis(60000));
+        }
+    });
+
     HttpServer::new(|| App::new()
             .service(
                 web::resource("/",).to(index)
---
+++ src/services/mod.rs
@@ -0,0 +1,11 @@
+use reqwest;
+
+
+pub mod twitch_service {
+
+
+    pub fn is_channel_online(channel_name: &str) -> bool {
+        let stream = reqwest::get("");
+        false
+    }
+}
