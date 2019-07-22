3798dc0316a4ead950cdd4b82147140041c3c978
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2019-07-22 21:59:11 +0200

    Add a thread that checks the twitch api

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,40 @@ mod services;
 
 use actix_files as fs;
 use actix_web::*;
-use std::thread;
-use std::time::Duration;
+use std::{
+    thread,
+    time::Duration,
+    sync::mpsc
+};
+use serde::Deserialize;
+use serde_json;
 
 use crate::controllers::pages_controller::*;
-use crate::services::twitch_service;
+use crate::services::twitch_service::*;
+
+const STREAMERS_JSON : &'static str = include_str!("../streamers.json");
+
+#[derive(Deserialize)]
+struct Streamers {
+    youtube: Vec<String>,
+    twitch: Vec<String>
+}
 
 fn main() -> std::io::Result<()> {
-    thread::spawn(|| {
+    dotenv::dotenv().expect("Failed to read .env file");
+    let streamers_list : Streamers = serde_json::from_str(STREAMERS_JSON)?;
+    let twitch_streamers_id_list = get_info_from_usernames(streamers_list.twitch)
+        .expect("Could not get twitch streamers id's").get_ids();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
         loop {
             println!("Fetching info");
-            twitch_service::is_channel_online("nietrickbroers");
+
+            get_online_channels(&twitch_streamers_id_list)
+            .map(|i| dbg!(i));
+
             thread::sleep(Duration::from_millis(60000));
         }
     });
@@ -27,7 +50,7 @@ fn main() -> std::io::Result<()> {
                 web::resource("/streamers",).to(streamers)
             )
             .service(
-                fs::Files::new("/resources", "./resources")
+                fs::Files::new("/resources", "../")
                     .show_files_listing()
             )
     )
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,11 +1,128 @@
-use reqwest;
-
-
 pub mod twitch_service {
+    use serde::{Deserialize, Serialize};
+    use serde_json::value::Value;
+    use std::error::Error;
+    use reqwest::{
+        Client,
+        header::HeaderValue
+    };
 
+    const BASE_URL: &str = "https://api.twitch.tv/kraken/";
 
-    pub fn is_channel_online(channel_name: &str) -> bool {
-        let stream = reqwest::get("");
-        false
+    #[derive(Deserialize, Debug)]
+    pub struct TwitchUserList{
+        users: Vec<TwitchUser>
+    }
+
+    impl TwitchUserList {
+        pub fn get_ids(self) -> Vec<String> {
+            self.users.iter().map(|i|i.id.to_owned()).collect()
+        }
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct TwitchUser {
+        #[serde(rename="_id")]
+        id: String,
+        bio: Value,
+        created_at: String,
+        display_name: String,
+        logo: String,
+        name: String,
+        updated_at: String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct Channel {
+        _id: i64,
+        broadcaster_language: String,
+        created_at: String,
+        display_name: String,
+        followers: i64,
+        game: String,
+        language: String,
+        logo: String,
+        mature: bool,
+        name: String,
+        partner: bool,
+        profile_banner: String,
+        profile_banner_background_color: String,
+        status: String,
+        updated_at: String,
+        url: String,
+        video_banner: String,
+        views: i64,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct Preview {
+        large: String,
+        medium: String,
+        small: String,
+        template: String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct StreamList {
+        streams: Vec<Streams>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct Streams {
+        _id: i64,
+        average_fps: i64,
+        channel: Channel,
+        created_at: String,
+        delay: i64,
+        game: String,
+        is_playlist: bool,
+        preview: Preview,
+        video_height: i64,
+        viewers: i64,
+    }
+
+    pub fn get_online_channels(channel_ids: &Vec<String>) -> Result<StreamList, Box<dyn Error>> {
+        let response = Client::new()
+            .get((BASE_URL.to_owned() + "/streams?channel=" + &channel_ids.join(",")).as_str())
+            .header("Client-ID", get_client_id_header_value() )
+            .header( reqwest::header::ACCEPT, "application/vnd.twitchtv.v5+json")
+            .send()?
+            .json()?;
+        Ok(response)
+    }
+
+    pub fn get_info_from_usernames(usernames: Vec<String>) -> Result<TwitchUserList, Box<dyn Error>> {
+        let response: TwitchUserList = Client::new()
+            .get((BASE_URL.to_owned() + "/users?login=" + &usernames.join(",").as_str()).as_str())
+            .header("Client-ID", get_client_id_header_value() )
+            .header( reqwest::header::ACCEPT, "application/vnd.twitchtv.v5+json")
+            .send()?
+            .json()?;
+        Ok(response)
+    }
+
+    fn get_client_id() -> String {
+        let default : String = "xxx".to_owned();
+
+        let env_client_id = std::env::var("TWITCH_CLIENT_ID");
+
+        let client_id : &String = match env_client_id {
+            Ok(ref id) => id,
+            Err(e) => {
+                println!("Could not read ({})", e);
+                &default
+            }
+        };
+        client_id.to_string()
+    }
+    fn get_client_id_header_value() -> HeaderValue {
+        match HeaderValue::from_str(&get_client_id()){
+            Ok(val) => {
+                val
+            }
+            _ => {
+                panic!("Value from client id could not be converted.")
+            }
+        }
     }
 }
